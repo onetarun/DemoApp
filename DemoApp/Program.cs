8ff2008a1338ba8ac3538bcf4e846135b3ede55a
Tarun Saini <onetarun@outlook.com>
2026-07-09 07:32:33 +0530

    Add Context

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -1,5 +1,6 @@
 using Azure.Identity;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 
 builder.Services.AddControllersWithViews();
